cca7036e066f3cf37df5c8665f6d4854a3b8a7e0
ray-anon <siddikshetty12@gmail.com>
2023-11-16 09:08:38 +0530

    last commit

--- whac-a-mole/index.js
+++ whac-a-mole/index.js
@@ -1,8 +1,12 @@
 const grid = document.querySelector(".grid");
 const timeLeft = document.querySelector(".time-left");
 const score = document.querySelector(".result");
+let count = 0;
 let squares = [];
+let molePosition;
 let result = 0;
+let currentTime = 60;
+let TimerId;
 for (let i = 0; i < 9; i++) {
   squares[i] = document.createElement("div");
   squares[i].setAttribute("id", i);
@@ -10,11 +14,31 @@ for (let i = 0; i < 9; i++) {
   grid.append(squares[i]);
 }
 function randomSqaure() {
-   squares.forEach((square) => {
-    square.classList.remove('mole');
-   })
-  let ranomPosition = squares[Math.floor(Math.random() * 9)];
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  molePosition = Math.floor(Math.random() * 9);
+  let ranomPosition = squares[molePosition];
   ranomPosition.classList.add("mole");
 }
 
-setInterval(randomSqaure, 1000);
+for (let i = 0; i < 9; i++) {
+  squares[i].addEventListener("click", Whacked);
+}
+function Whacked() {
+  if (this.id == molePosition) {
+    count++;
+    score.innerText = count;
+  }
+}
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(TimerId);
+    alert("your score is " + count);
+  }
+}
+TimerId = setInterval(randomSqaure, 1000);
+let countDownTimerId = setInterval(countDown, 1000);
